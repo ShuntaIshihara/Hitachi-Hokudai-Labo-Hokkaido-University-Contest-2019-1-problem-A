16a496da0146adaa1cdfc948a0a5702f6263314e
148rashunta <ishunta148raw01f826@gmail.com>
2019-11-20 16:13:26 +0900

    compleate

--- main.py
+++ main.py
@@ -69,14 +69,26 @@ for i in range(T):
 	for j in range(Nnew):
 		new_id, dst = map(int, input().split())
 		oder_time[new_id] = i
-		oder_list[i].append((new_id, dst))
+		oder_list[i].append((new_id, dst-1))
 
+#配達に向かう目的地をリストに入れる
+def get_destination(d, l):
+	for i in range(V):
+		if l[i]:
+			d.append(i)
 
+#時間s+1からtまでに注文された荷物を受け取る関数
+def get_luggage(l, s, t):
+	for i in range(s+1, t+1):
+		for oder in oder_list[i]:
+			l[oder[1]].append(oder[0])
+	
+	return t
 
 
 #配達場所までの最短経路になるようにソートする関数
-def search_shortest_route(start, start_index, d):
-	if start_index >= len(d):
+def get_route(start, start_index, d):
+	if start_index >= len(d)-1:
 		return
 
 	min = sys.maxsize
@@ -90,14 +102,9 @@ def search_shortest_route(start, start_index, d):
 	d[start_index] = d[v]
 	d[v] = w
 
-	search_shortest_route(v, start_index+1, d)
+	get_route(d[start_index], start_index+1, d)
 
-#車に積んである荷物を記録していく
-#key: 目的地, value: 注文idのリスト
-luggage = {key: [] for key in range(V)}	
 LEVEL = 4		#読みきる深さ
-next_move = -1	#車が次に取る行動
-
 #最適解を探索する
 #t: 時間,  level: 読んでいる深さ, vehicle: 車の位置, score: 得点, shipping: 最後にお店に立ち寄った時間, dst_list: 配達に向かう順番, luggage: 荷物 
 def search(t, level, vehicle, score, shipping, dst_list, luggage):
@@ -105,86 +112,61 @@ def search(t, level, vehicle, score, shipping, dst_list, luggage):
 	if t >= T:
 		return score
 
+	#levelが読みきる深さになったとき
+	if level >= LEVEL:
+		return score
+
+	luggage_copy = copy.deepcopy(luggage)
+	dst_list_copy = copy.copy(dst_list)
 	#車がお店にいるとき
 	if vehicle == 0:
-		#level >= 読み切る深さ のとき得点を計算して返す
-		if level >= LEVEL:
-			return score
 
 		#時間tまでに受けた注文idを受け取る
-		for i in range(sipping+1, t+1):
-			for oder in oder_list[i]:
-				luggage[oder[1]].append(oder[0])
-		luggage_copy = copy.deepcopy(luggage)
+		shipping = get_luggage(luggage_copy, shipping, t)
 
 		#配達場所(複数の目的地)までの最短経路を計算する
 		#もしdst_listが空ではなかったら空にする
-		if dst_list != None:
-			dst_list.clear()
+		if dst_list_copy != None:
+			dst_list_copy.clear()
 
 		#配達に向かう場所を記憶する
-		for i in range(V):
-			if luggage[i]:
-				dst_list.append(i)
+		get_destination(dst_list_copy, luggage_copy)
 
 		#dst_listが最短経路でソートされる
-		search_shortest_route(0, 0, dst_list)		
-		dst_list_copy = copy.copy(dst_list)
+		get_route(0, 0, dst_list_copy)
 
 		#comp = search(配達に行く場合)
-		max_score = search(t+shortest_time[vehicle][dst_list[0]], level+1, dst_list[0], score, t, dst_list_copy, luggage_copy)
-		if level == 0:
-			next_move = dst_list[0]
+		max_score = search(t+shortest_time[vehicle][dst_list[0]], level+1, dst_list[0], score, shipping, dst_list_copy, luggage_copy)
 		#comp = search(店にとどまる場合)
-		comp = search(t+1, level+1, vehicle, score, t, dst_list_copy, luggage_copy)
+		comp = search(t+1, level+1, vehicle, score, shipping, dst_list_copy, luggage_copy)
 		#comp > max のとき now_score = comp
 		if comp > max_score:
 			max_score = comp
-			if level == 0:
-				next_move = -1
 		#return max
 		return max_score
 
 
 	#車が今積んでいるすべての荷物を配達完了したとき
-	elif dst_list.index(vehicle) == len(dst_list) - 1:
-		for i in luggage[vehicle]:
+	elif dst_list_copy.index(vehicle) == len(dst_list_copy) - 1:
+		for i in luggage_copy[vehicle]:
 			#得点計算
 			waitinTime = t - oder_time[i]
 			score += T*T - waitingTime*waitingTime
-		luggage[vehicle].clear()
-
-		#level >= 読み切る深さ のとき得点を計算して返す
-		if level >= LEVEL:
-			return score:
+		luggage_copy[vehicle].clear()
 
 		#return search(店に戻る, 計算した得点を引数に渡す)
-		dst_list_copy = copy.copy(dst_list)
-		luggage_copy = copy.deepcopy(luggage)
-		if level == 0:
-			next_move = 0
 		return search(t+shortest_time[vehicle][0], level+1, 0, score, shipping, dst_list_copy, luggage_copy)
 
 	#車が途中の配達まで完了したとき
 	else:
-		for i in luggage[vehicle]:
+		for i in luggage_copy[vehicle]:
 			#得点計算
 			waitingTime = t - oder_time[i]
 			score += T*T - waitingTime*waitingTime
-		luggage[vehicle].clear()
-
-		#level >= 読み切る深さ のとき得点を計算して返す
-		if level >= LEVEL:
-			return score
-
-		dst_list_copy = copy.copy(dst_list)
-		luggage_copy = copy.deepcopy(luggage)
+		luggage_copy[vehicle].clear()
 
 		#comp = search(次の配達場所に向かう, 計算した得点を引数に渡す)
 		max_score = search(t+shortest_time[vehicle][dst_list[dst_list.index(vehicle)+1]], level+1, dst_list[dst_list.index(vehicle)+1], score, shipping, dst_list_copy, luggage_copy)
-		
-		if level == 0:
-			next_move = dst_list[dst_list.index(vehicle)+1]
 
 		#comp = search(店に戻る, 計算した得点を引数に渡す)
 		comp = search(t+shortest_time[vehicle][0], level+1, 0, score, shipping, dst_list_copy, luggage_copy)
@@ -194,9 +176,86 @@ def search(t, level, vehicle, score, shipping, dst_list, luggage):
 		#return max
 		return max_score
 
-
+#車に積んである荷物を記録していく
+#key: 目的地, value: 注文idのリスト
+real_l = {key: [] for key in range(V)}
+#車の目的地を記録していく
+real_d = []
+#index
+index = 0
+#次の行動
+next_move = -1
+#次の目的地
+next_dst = 0
+#車の現在地
+real_v = 0
+#最後に店に訪れた時間
+shipping_time = -1
+#次の頂点につくまでのカウンター
+count = 0
 # insert your code here to get more meaningful output
 # all stay
 for i in range(T) :
-	#返り値: next_move
-	start_search(i, score)
+	#次の目的地と車の現在地が等しいとき
+	if real_v == next_dst:
+		count = 0
+		#車が店にいるとき
+		if real_v == 0:
+			index = 0
+			#荷物を受け取る関数
+			shipping_time = get_luggage(real_l, shipping_time, i)
+			#新しく目的地のリストを作る
+			if real_d != None:
+				real_d.clear()
+			get_destination(real_d, real_l)
+			#複数の目的地のルートを受け取る関数
+			get_route(0, 0, real_d)
+			#とどまる場合
+			max_score = search(i+1, 0, real_v, 0, shipping_time, real_d, real_l)
+			next_dst = 0
+			next_move = -1
+			#いく場合
+			comp = search(i+shortest_time[real_v][real_d[index]], 0, real_d[index], 0, shipping_time, real_d, real_l)
+			if comp > max_score:
+				next_dst = real_d[index]
+				next_move = shortest_route[real_v][next_dst][0]
+				index += 1
+		#車が今ある荷物をすべて配達したとき
+		elif index == len(real_d) - 1:
+			real_d.clear()
+			next_dst = 0
+			next_move = shortest_route[real_v][next_dst][0]
+		#車が途中の配達場所まで配達完了したとき
+		else:
+			#店にもどる
+			max_score = search(i+shortest_time[real_v][0], 0, 0, 0, shipping_time, real_d, real_l)
+			next_dst = 0
+			next_move = shortest_route[real_v][next_dst][0]
+			#次の目的地に行く
+			comp = search(i+shortest_time[real_v][real_d[index]], 0, real_d[index], 0, shipping_time, real_d, real_l)
+			if comp > max_score:
+				next_dst = real_d[index]
+				next_move = shortest_route[real_v][next_dst][0]
+				index += 1
+
+		#次の行動を出力する
+		if next_move != -1:
+			count += 1
+			print(next_move+1)
+		else:
+			print(next_move)
+
+	#次の頂点まで移動中のとき
+	else:
+		if count == shortest_time[real_v][next_move]-1:
+			if next_move == next_dst:
+				real_v = next_dst
+				count += 1
+				print(next_move+1)
+				continue
+			count = 0
+			real_v = next_move
+			next_move = shortest_route[real_v][next_dst][0]
+
+		count += 1
+		print(next_move+1)
